b9b612633e159d18ead32c71ba51e219af67177f
Danny <hi@dannyspina.com>
2024-02-19 23:44:37 +0100

    add custom splash screen + updates

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -2,7 +2,7 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     -- This configuration is added to use eslint as formatter on save. There where conflicts between eslint and formatter in herole project
-    servers = { eslint = {}, volar = {} },
+    servers = { eslint = {}, volar = {}, yamlls = {} },
     setup = {
       eslint = function()
         require("lazyvim.util").lsp.on_attach(function(client)
@@ -12,6 +12,19 @@ return {
             client.server_capabilities.documentFormattingProvider = false
           end
         end)
+      end,
+      yamlls = function()
+        require("lspconfig").yamlls.setup({
+          settings = {
+            yaml = {
+              format = {
+                enable = true,
+                singleQuote = false,
+                bracketSpacing = false,
+              },
+            },
+          },
+        })
       end,
     },
   },
